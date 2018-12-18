f66c1f74f1ab57a08f868dbde67b0d96277f56e3
QMaximillian <quinnlashinsky@gmail.com>
2018-12-18 01:48:13 -0500

    House form creation

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,3 +1,27 @@
+const baseHomeUrl = "http://localhost:3001/homes"
+
 export const fetchUser = () =>  {
-  return fetch('https://localhost:3001/users').then(resp => resp,json())
+  return fetch('https://localhost:3001/users').then(resp => resp.json())
+}
+
+export const fetchCreateHome = (e, body) => {
+  e.preventDefault()
+  return fetch(baseHomeUrl, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+}
+
+const headers = (method, body) => {
+  return {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json'
+
+    },
+    body: JSON.stringify(body)
+  }
 }
--- src/components/HouseCreateForm.js
+++ src/components/HouseCreateForm.js
@@ -1,16 +1,67 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import '../App.css'
+import { fetchCreateHome } from '../adapters/index.js'
 
 class HouseCreateForm extends Component {
+
+  state = {
+    // TODO:
+      // Structure home state with correct params for Rails API
+      // Check roomates to see if any other users exist with those emails. Ask them if they want to join the house
+      // When they agree add a home_id to associate them to a house
+      // Clean up the CSS and form submit page for creating a house
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    }, () => console.log(this.state))
+  }
+
+
    render() {
      return (
-        <div className="house-create-form-grid">
-          <div>
-          Hello
-          </div>
-        </div>
+      <Fragment>
+        <label
+          id="label-center"
+          >Create Your House
+        </label>
+          <form
+            onChange={this.handleChange}
+            className="house-form">
+            <label
+              id="house-item-label1"
+              >Roomate E-Mail
+            </label>
+            <input
+              name="roomate1email"
+              className="house-item .house-item1"/>
+            <label
+              id="house-item-label2">Roomate E-Mail
+            </label>
+            <input
+              name="roomate2email"
+              className="house-item2 house-item"/>
+            <label>Roomate E-Mail</label>
+            <input
+              name="roomate3email"
+              className="house-item3 house-item"/>
+            <label>House Name</label>
+            <input
+              name="houseName"
+              className="house-item4 house-item"/>
+            <label>House Address</label>
+            <input
+              name="houseAddress"
+              className="house-item5 house-item"/>
+
+            <button
+              className="house-item house-submit"
+              onClick={(e) => fetchCreateHome(e)}>Submit</button>
+          </form>
+      </Fragment>
      )
    }
  }
